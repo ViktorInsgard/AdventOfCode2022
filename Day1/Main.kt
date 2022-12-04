cb6f793904176051f256113b539706e6ab1f013f
ViktorInsgard <viktor.insgard@gmail.com>
2022-12-04 20:28:09 +0100

    Day 1 complete!

---
+++ Day1/Main.kt
@@ -0,0 +1,38 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = File(args[0]).readText()
+    println(findAnswerA(input))
+    println(findAnswerB(input))
+}
+
+fun findAnswerA(input: String) : Int {
+    return input
+        .split("\r\n\r\n")
+        .maxBy { s ->
+            s.split("\r\n").sumOf { i ->
+                i.toInt()
+            }
+        }
+        .split("\r\n")
+        .sumOf { i ->
+            i.toInt()
+        }
+}
+
+fun findAnswerB(input: String) : Int {
+    return input
+        .split("\r\n\r\n")
+        .sortedBy { s ->
+            s.split("\r\n").sumOf { i ->
+                i.toInt()
+            }
+        }
+        .reversed()
+        .take(3)
+        .sumOf { s ->
+            s.split("\r\n").sumOf { i ->
+                i.toInt()
+            }
+        }
+}
